c15c52e7fba7b426446936d88a657be3179cf2bb
ktulhoo86 <ktulhoo.pyneng@yandex.ru>
2021-09-08 22:25:28 +0300

    2_if2.py Done

--- 2_if2.py
+++ 2_if2.py
@@ -10,17 +10,29 @@
 * Если строки одинаковые, вернуть 1
 * Если строки разные и первая длиннее, вернуть 2
 * Если строки разные и вторая строка 'learn', возвращает 3
-* Вызвать функцию несколько раз, передавая ей разные праметры 
+* Вызвать функцию несколько раз, передавая ей разные параметры
   и выводя на экран результаты
 
 """
 
-def main():
+
+def main(string1, string2):
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+    if type(string1) != str and type(string2) != str:
+        return 0
+    elif string1 == string2:
+        return 1
+    elif len(string1) > len(string2):
+        return 2
+    elif string1 != string2 and string2 == 'learn':
+        return 3
+
+
 if __name__ == "__main__":
-    main()
+    print(main(True, False))
+    print(main('box', 'box'))
+    print(main('boxx', 'box'))
+    print(main('boxx', 'learn'))
